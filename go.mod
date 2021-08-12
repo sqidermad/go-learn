c54c743f63b3830babe74f81f1e50e1113af60ca
Maria Vanessa Salim Siburian <mariavanessasalim@gmail.com>
2021-08-12 14:02:18 +0700

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module source-code
+
+go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/shopspring/decimal v1.2.0
+)
---
+++ main.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/shopspring/decimal"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+)
+
+var db *gorm.DB
+var err error
+
+// Product is a representation of a product
+type Product struct {
+	ID    int             `form:"id" json:"id"`
+	Code  string          `form:"code" json:"code"`
+	Name  string          `form:"name" json:"name"`
+	Price decimal.Decimal `form:"price" json:"price" sql:"type:decimal(16,2);"`
+}
+
+// Result is an array of product
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func main() {
+	//bikin koneksi
+	db, err = gorm.Open("mysql", "root:@/go_rest_api_crud?charset=uf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection Failed", err)
+	} else {
+		log.Println("Connection Established")
+	}
+
+	db.AutoMigrate(&Product{}) //migrasi data product ke database
+	handleRequests()
+}
+
+func handleRequests() {
+	log.Println("Start the development server at http://127.0.0.1:9999")
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+
+		res := Result{Code: 404, Message: "Method not found"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
+	myRouter.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		res := Result{Code: 403, Message: "Method not allowed"}
+		response, _ := json.Marshal(res)
+		w.Write(response)
+	})
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/api/products", createProduct).Methods("POST")
+	myRouter.HandleFunc("/api/products", getProducts).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
+	myRouter.HandleFunc("/api/products/{id}", deleteProduct).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":9999", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	payloads, _ := ioutil.ReadAll(r.Body) //nangkep payload
+
+	var product Product
+	json.Unmarshal(payloads, &product)
+
+	db.Create(&product)
+
+	res := Result{Code: 200, Data: product, Message: "Success create product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: get products")
+
+	products := []Product{}
+	db.Find(&products)
+
+	res := Result{Code: 200, Data: products, Message: "Success get products"}
+	results, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(results)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) //get product pake ID
+	productID := vars["id"]
+
+	var product Product
+
+	db.First(&product, productID)
+
+	res := Result{Code: 200, Data: product, Message: "Success get product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"] //cari ID
+
+
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var productUpdates Product
+	json.Unmarshal(payloads, &productUpdates)
+
+
+	var product Product
+	db.First(&product, productID)
+
+	//update datanya pake payload yang dikirim
+	db.Model(&product).Updates(productUpdates)
+
+	res := Result{Code: 200, Data: product, Message: "Success update product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	var product Product
+
+	db.First(&product, productID)
+	db.Delete(&product)
+
+	res := Result{Code: 200, Message: "Success delete product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
